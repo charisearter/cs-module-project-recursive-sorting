6300c5a416b7cb9cca8da12c2ac2da01dbc891dc
Charise Arter <mine@Charises-MacBook-Pro.local>
2020-08-19 12:55:26 -0500

    done but have error

--- src/sorting/sorting.py
+++ src/sorting/sorting.py
@@ -2,8 +2,21 @@
 def merge(arrA, arrB):
     elements = len(arrA) + len(arrB)
     merged_arr = [0] * elements
-
     # Your code here
+
+    while len(arrA) > 0 and len(arrB) > 0:  # if both arrays are > 0 basecase
+        if arrA[0] < arrB[0]:  # if arrA is less than arrB index 0
+            # append popped 0 index of array A to merged
+            merged_arr.append(arrA.pop(0))
+        else:
+            # append popped 0 index of B array to merged
+            merged_arr.append(arrB.pop(0))
+    while len(arrA) > 0:
+        # append popped 0 index of array A to merged
+        merged_arr.append(arrB.pop(0))
+    while len(arrB) > 0:
+        # append popped 0 index of array B to merged
+        merged_arr.append(arrB.pop(0))
 
     return merged_arr
 
@@ -13,12 +26,12 @@ def merge(arrA, arrB):
 def merge_sort(arr):
     # Your code here
     if len(arr) > 1:  # if the length of the array is greater than 1
-        mid = len(arr)/2  # split at the middle
-        lefthalf = arr[:mid]  # the left half is everything from start to mid
-        righthalf = arr[mid:]  # the right half is everything from mid to end
+        mid = len(arr)//2  # split at the middle
+        l = arr[:mid]  # the left half is everything from start to mid
+        r = arr[mid:]  # the right half is everything from mid to end
 
-        merge_sort(lefthalf)  # keep going until it is 1 -> recursive call
-        merge_sort(righthalf)  # keep going until it is 1
+        # return using helper fn merge with merge_sort l and merge_sort r as arrA and arrB replacements
+        return merge(merge_sort(l), merge_sort(r))
     return arr
 
 # STRETCH: implement the recursive logic for merge sort in a way that doesn't
