9067fe6f82b7a9ea204d3c2663e5c689867c4e36
Charise Arter <mine@Charises-MacBook-Pro.local>
2020-08-19 12:19:18 -0500

    binary search sort of complete but not working not sure why

--- src/searching/searching.py
+++ src/searching/searching.py
@@ -1,14 +1,24 @@
 # TO-DO: Implement a recursive implementation of binary search
 def binary_search(arr, target, start, end):
-    # Your code here
+    while start < end:
+        middle = (start + end)//2  # // so it will be int not float
+        if target == middle:  # if target is middle
+            return middle  # return the middle
+        elif target < arr[middle]:  # if target is less than middle
+            # end = arr[:middle-1]  # remove right side of middle
+            return binary_search(arr, target, start, (middle - 1))
+        elif target > arr[middle]:  # if target is greater than array
+            # start = arr[middle+1:]  # remove the left side of middle
+            return binary_search(arr, target, (middle + 1), end)
+    return -1  # return this if not found
 
 
 # STRETCH: implement an order-agnostic binary search
-# This version of binary search should correctly find 
+# This version of binary search should correctly find
 # the target regardless of whether the input array is
 # sorted in ascending order or in descending order
-# You can implement this function either recursively 
+# You can implement this function either recursively
 # or iteratively
 def agnostic_binary_search(arr, target):
     # Your code here
-
+    pass
--- src/sorting/sorting.py
+++ src/sorting/sorting.py
@@ -5,24 +5,33 @@ def merge(arrA, arrB):
 
     # Your code here
 
-
     return merged_arr
 
 # TO-DO: implement the Merge Sort function below recursively
+
+
 def merge_sort(arr):
     # Your code here
+    if len(arr) > 1:  # if the length of the array is greater than 1
+        mid = len(arr)/2  # split at the middle
+        lefthalf = arr[:mid]  # the left half is everything from start to mid
+        righthalf = arr[mid:]  # the right half is everything from mid to end
 
-
+        merge_sort(lefthalf)  # keep going until it is 1 -> recursive call
+        merge_sort(righthalf)  # keep going until it is 1
     return arr
 
-# STRETCH: implement the recursive logic for merge sort in a way that doesn't 
+# STRETCH: implement the recursive logic for merge sort in a way that doesn't
 # utilize any extra memory
-# In other words, your implementation should not allocate any additional lists 
+# In other words, your implementation should not allocate any additional lists
 # or data structures; it can only re-use the memory it was given as input
+
+
 def merge_in_place(arr, start, mid, end):
+    pass
     # Your code here
 
 
 def merge_sort_in_place(arr, l, r):
     # Your code here
-
+    pass
